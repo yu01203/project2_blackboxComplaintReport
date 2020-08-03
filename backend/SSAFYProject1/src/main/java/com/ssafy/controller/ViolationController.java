fbc1c8eebac9fd3328c95b743be39ebf321fdde8
justin613 <justin613@naver.com>
2020-08-03 15:34:47 +0900

    S03P13A211-23 S03P13A211-28 | Back-End > Violation Modify, Apply Swagger

--- backend/SSAFYProject1/src/main/java/com/ssafy/controller/ViolationController.java
+++ backend/SSAFYProject1/src/main/java/com/ssafy/controller/ViolationController.java
@@ -1,9 +1,12 @@
 package com.ssafy.controller;
 
 import java.util.List;
-import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,56 +21,58 @@ import com.ssafy.domain.Violation;
 import com.ssafy.service.UserService;
 import com.ssafy.service.ViolationService;
 
+import io.swagger.annotations.ApiOperation;
+
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("api/violation")
 public class ViolationController {
+	private static final Logger logger = LoggerFactory.getLogger(NoticeController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 
 	@Autowired
 	UserService userService;
 	@Autowired
 	ViolationService violationService;
 
-	@GetMapping
-	public List<Violation> viewViolaions(@RequestBody Map<String, Object> param) throws Exception {
-		System.out.println("신고 정보 리스트 조회");
+	@ApiOperation(value = "모든 제보 정보를 리스트로 반환한다.", response = List.class)
+	@GetMapping("{userNo}")
+	public ResponseEntity<List<Violation>> viewViolaions(@PathVariable int userNo) throws Exception {
+		logger.debug("신고 리스트 조회 - 호출");
 
 		// 이메일로 회원번호 조회
-		int userNo = userService.detail(param.get("email").toString()).getUserNo();
-		List<Violation> violationList = violationService.viewViolationList(userNo);
-
-		return violationList;
+		return new ResponseEntity<List<Violation>>(violationService.viewViolationList(userNo), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "모든 제보 정보를 리스트로 반환한다.", response = Violation.class)
 	@GetMapping("{userNo}/{violationNo}")
-	public Violation viewViolation(@PathVariable int userNo, @PathVariable int violationNo) throws Exception {
-		System.out.println("신고 정보 조회");
-		
-		Violation violation = violationService.viewViolation(violationNo, userNo);
-		return violation;
+	public ResponseEntity<Violation> viewViolation(@PathVariable int userNo, @PathVariable int violationNo) throws Exception {
+		logger.debug("신고  조회 - 호출");
+		return new ResponseEntity<Violation>(violationService.viewViolation(violationNo, userNo), HttpStatus.OK);
 	}
 
 	@PostMapping
-	public String addViolation(@RequestBody Violation violation) throws Exception {
-		System.out.println("신고하기");
+	public ResponseEntity<String> addViolation(@RequestBody Violation violation) throws Exception {
+		logger.debug("신고 정보 삽입 - 호출");
 		
-		if(violationService.addViolation(violation) == 1) return "success";
-		return "fail";
+		if(violationService.addViolation(violation) == 1) return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		return new ResponseEntity<String>(FAIL, HttpStatus.NOT_ACCEPTABLE);
 	}
 
 	@PutMapping
-	public String modifyViolation(@RequestBody Violation violation) throws Exception {
-		System.out.println("신고 정보 수정");
+	public ResponseEntity<String> modifyViolation(@RequestBody Violation violation) throws Exception {
+		logger.debug("신고 정보 수정 - 호출");
 		
-		if(violationService.modifyViolation(violation) == 1) return "success";
-		return "fail";
+		if(violationService.modifyViolation(violation) == 1) return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		return new ResponseEntity<String>(FAIL, HttpStatus.NOT_ACCEPTABLE);
 	}
 
 	@DeleteMapping("{userNo}/{violationNo}")
-	public String removeViolation(@PathVariable int violationNo, @PathVariable int userNo) throws Exception {
-		System.out.println("신고 정보 삭제");
+	public ResponseEntity<String> removeViolation(@PathVariable int violationNo, @PathVariable int userNo) throws Exception {
+		logger.debug("신고 정보 삭제 - 호출");
 		
-		if(violationService.removeViolation(violationNo, userNo) == 1) return "success";
-		return "fail";
+		if(violationService.removeViolation(violationNo, userNo) == 1) return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		return new ResponseEntity<String>(FAIL, HttpStatus.NOT_ACCEPTABLE);
 	}
 }
