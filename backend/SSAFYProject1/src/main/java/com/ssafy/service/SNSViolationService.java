4cc177dd5eeb5e22ca91e6f0eafaa741bfe97dba
justin613 <justin613@naver.com>
2020-08-04 14:26:02 +0900

    S03P13A211-33 | Back-End > SNS Violation

---
+++ backend/SSAFYProject1/src/main/java/com/ssafy/service/SNSViolationService.java
@@ -0,0 +1,17 @@
+package com.ssafy.service;
+
+import java.util.List;
+
+import com.ssafy.domain.Violation;
+
+public interface SNSViolationService {
+	Violation viewViolation(int violationNo, int userNo) throws Exception;
+
+	List<Violation> viewViolationList(int userNo) throws Exception;
+
+	int addViolation(Violation violation) throws Exception;
+
+	int modifyViolation(Violation violation) throws Exception;
+
+	int removeViolation(int violationNo, int userNo) throws Exception;
+}
---
+++ backend/SSAFYProject1/src/main/java/com/ssafy/service/SNSViolationServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ssafy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.domain.Violation;
+import com.ssafy.repository.ViolationRepository;
+
+@Service
+public class SNSViolationServiceImpl implements SNSViolationService {
+	
+	@Autowired
+	ViolationRepository repository;
+
+	@Override
+	public Violation viewViolation(int violationNo, int userNo) throws Exception {
+		return repository.select(violationNo, userNo);
+	}
+
+	@Override
+	public List<Violation> viewViolationList(int userNo) throws Exception {
+		return repository.selectAll(userNo);
+	}
+
+	@Override
+	public int addViolation(Violation violation) throws Exception {
+		return repository.insert(violation);
+	}
+
+	@Override
+	public int modifyViolation(Violation violation) throws Exception {
+		return repository.update(violation);
+	}
+
+	@Override
+	public int removeViolation(int violationNo, int userNo) throws Exception {
+		return repository.delete(violationNo, userNo);
+	}
+
+}
