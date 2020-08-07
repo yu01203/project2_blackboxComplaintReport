6e858b3b224eafd0b9cf11b99bc7b9145878af3f
조원빈 <cwb5116@naver.com>
2020-08-07 12:11:45 +0900

    Colour Changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,7 @@
       <br />
     </div>
 
-    <footer class="mastfoot m-0 border-top border-info" style="background-color: #ffffff;">
+    <footer class="mastfoot m-0 border-top" style="background-color: #ffffff;">
       <div class="text-center">
         <p class="mt-3">SSAFY 3기 공통 Project 2반 11조 나도내가무섭조</p>
       </div>
