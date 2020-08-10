e0d24d884fb8c8c5761472ede0cf3cf3a1df1106
김남규 <kngking22@naver.com>
2020-08-11 02:01:31 +0900

    S03P13A211-66 | Back-end > threeGraph

--- backend/SSAFYProject1/src/main/java/com/ssafy/service/SituationService.java
+++ backend/SSAFYProject1/src/main/java/com/ssafy/service/SituationService.java
@@ -3,7 +3,11 @@ package com.ssafy.service;
 import java.util.List;
 
 import com.ssafy.domain.Situation;
+import com.ssafy.domain.Test;
 
 public interface SituationService {
-	List<Situation> selectAll(String date) throws Exception;
+	//List<Situation> selectAll(String date) throws Exception;
+	
+	List<List<Test>> selectAll() throws Exception;
+	List<List<Test>> selectByUser(int userno) throws Exception;
 }
--- backend/SSAFYProject1/src/main/java/com/ssafy/service/SituationServiceImpl.java
+++ backend/SSAFYProject1/src/main/java/com/ssafy/service/SituationServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ssafy.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ssafy.domain.Situation;
+import com.ssafy.domain.Test;
 import com.ssafy.repository.SituationRepository;
 
 @Service
@@ -13,9 +15,32 @@ public class SituationServiceImpl implements SituationService {
 	@Autowired
 	SituationRepository repo;
 	
+//	@Override
+//	public List<Situation> selectAll(String date) throws Exception {
+//		return repo.selectAll(date);
+//	}
 	@Override
-	public List<Situation> selectAll(String date) throws Exception {
-		return repo.selectAll(date);
+	public List<List<Test>> selectAll() throws Exception {
+		
+		List<List<Test>> abc = new ArrayList<List<Test>>();
+
+		abc.add(repo.select0());
+		abc.add(repo.select1());
+		abc.add(repo.select2());	
+		
+		return abc;
+	}
+	
+	@Override
+	public List<List<Test>> selectByUser(int userno) throws Exception {
+		
+		List<List<Test>> abc = new ArrayList<List<Test>>();
+
+		abc.add(repo.selectByUser0(userno));
+		abc.add(repo.selectByUser1(userno));
+		abc.add(repo.selectByUser2(userno));	
+	
+		return abc;
 	}
 
 }
