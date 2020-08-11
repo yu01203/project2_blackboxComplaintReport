b1b0cb534ca43d51dcc554442baa4152a2bf81f1
최정원 <jungwon9156@khu.ac.kr>
2020-08-11 15:51:05 +0900

    feature-search

--- frontend/src/views/CaseList.vue
+++ frontend/src/views/CaseList.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <h1 v-if="local_violationitems.length != 0" class="text-center mb-3">제보목록</h1>
-    <b-container class="bv-example-row">
-      <b-row>
-        <Case
-          v-for="violationitem in local_violationitems"
-          :key="violationitem.violationNo"
-          :violationitem="violationitem"
-        />
-      </b-row>
-    </b-container>
+    <div>
+      <h1 v-if="local_violationitems.length != 0" class="text-center mb-3">제보목록</h1>
+      <b-container class="bv-example-row">
+        <b-row>
+          <Case
+            v-for="violationitem in local_violationitems"
+            :key="violationitem.violationNo"
+            :violationitem="violationitem"
+          />
+        </b-row>
+      </b-container>
+    </div>
+    <div>
+      <h1 v-if="local_violationitems.length != 0" class="text-center mb-3">제보목록 서치</h1>
+
+      <input type="text" v-model="searchText" @keydown.enter="searchOnlist" />
+      <b-container class="bv-example-row">
+        <b-row>
+          <Case
+            v-for="violationitem in searchviolationitems"
+            :key="violationitem.violationNo"
+            :violationitem="violationitem"
+          />
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -30,6 +46,8 @@ export default {
   data() {
     return {
       local_violationitems: [],
+      searchText: "",
+      searchviolationitems: [],
     };
   },
   created() {
@@ -49,6 +67,23 @@ export default {
   methods: {
     getVio() {
       this.$store.dispatch("getViolations");
+    },
+    searchOnlist() {
+      const emptyItems = new Array();
+      this.searchviolationitems = emptyItems;
+      for (var i in this.local_violationitems) {
+        console.log(this.local_violationitems[i]);
+        if (
+          this.local_violationitems[i].contents.indexOf(this.searchText) !=
+            -1 ||
+          this.local_violationitems[i].item.indexOf(this.searchText) != -1 ||
+          this.local_violationitems[i].spot.indexOf(this.searchText) != -1 ||
+          this.local_violationitems[i].carNum.indexOf(this.searchText) != -1 ||
+          this.local_violationitems[i].address.indexOf(this.searchText) != -1
+        ) {
+          this.searchviolationitems.push(this.local_violationitems[i]);
+        }
+      }
     },
   },
 };
