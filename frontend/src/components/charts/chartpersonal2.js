a4c38c01bdabd95640d6baa30f4f0f0c3200a2e3
김남규 <kngking22@naver.com>
2020-08-17 16:25:21 +0900

    S03P13A211-107 | Frontend > Graph Height More

--- frontend/src/components/charts/chartpersonal2.js
+++ frontend/src/components/charts/chartpersonal2.js
@@ -6,6 +6,7 @@ export default {
   data() {
     return {
       items: [],
+      maxNum: -1,
     };
   },
 
@@ -17,6 +18,13 @@ export default {
         if (data != null) {
           this.$store.state.situation = data;
           this.items = data;
+
+          for (let index = 0; index < this.items[1].length; index++) {
+            if (this.maxNum < this.items[1][index].count) {
+              this.maxNum = this.items[1][index].count;
+            }
+          }
+
           this.makegraph();
         } else {
           alert(" 실패했습니다.");
@@ -28,42 +36,62 @@ export default {
   },
   methods: {
     makegraph() {
-      this.renderChart({
-        labels: [
-          "1월",
-          "2월",
-          "3월",
-          "4월",
-          "5월",
-          "6월",
-          "7월",
-          "8월",
-          "9월",
-          "10월",
-          "11월",
-          "12월",
-        ],
-        datasets: [
-          {
-            label: "교통 사고 현황",
-            backgroundColor: "#ff8888",
-            data: [
-              this.items[1][0].count,
-              this.items[1][1].count,
-              this.items[1][2].count,
-              this.items[1][3].count,
-              this.items[1][4].count,
-              this.items[1][5].count,
-              this.items[1][6].count,
-              this.items[1][7].count,
-              this.items[1][8].count,
-              this.items[1][9].count,
-              this.items[1][10].count,
-              this.items[1][11].count,
+      this.renderChart(
+        {
+          labels: [
+            "1월",
+            "2월",
+            "3월",
+            "4월",
+            "5월",
+            "6월",
+            "7월",
+            "8월",
+            "9월",
+            "10월",
+            "11월",
+            "12월",
+          ],
+          datasets: [
+            {
+              label: "교통 사고 현황",
+              backgroundColor: "#ff8888",
+              data: [
+                this.items[1][0].count,
+                this.items[1][1].count,
+                this.items[1][2].count,
+                this.items[1][3].count,
+                this.items[1][4].count,
+                this.items[1][5].count,
+                this.items[1][6].count,
+                this.items[1][7].count,
+                this.items[1][8].count,
+                this.items[1][9].count,
+                this.items[1][10].count,
+                this.items[1][11].count,
+              ],
+            },
+          ],
+        },
+        {
+          legend: {
+            //hides the legend
+            display: false,
+          },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  max: this.maxNum + 10,
+                  // max: Math.max(...this.items[0].data) + 10,
+                  // display: false,
+                  beginAtZero: true,
+                },
+              },
             ],
           },
-        ],
-      });
+        }
+      );
     },
   },
 };
