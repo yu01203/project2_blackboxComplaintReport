298664623446ce2b83eb227205d6d209a02bca9a
박세종 <tpwhd95@gmail.com>
2020-08-20 11:13:39 +0900

    S03P13A211-118 | Frontend > HowToUse

--- frontend/src/views/HowToUse.vue
+++ frontend/src/views/HowToUse.vue
@@ -4,28 +4,44 @@
     <h1 class="text-center mb-3 bolder">사이트 이용 방법</h1>
     <div class="divdiv">
       <h2 style="background-color: #FFFFF0;">1. 민원 처리 상태 관리</h2>
-      <img src="@/assets/logos/HowToUse1.png" alt="HowToUse1" style="width: 100%;" />
+      <img
+        src="@/assets/logos/HowToUse1.png"
+        alt="HowToUse1"
+        style="width: 100%; border: solid 2px"
+      />
       <p>
         1) 초기 상태는
-        <span style="color: #6c757d">신고 미접수</span>로 아직 스마트 국민제보 사이트에 민원을 접수하기 전 단계입니다.
+        <span style="background-color: #FFFFF0;">신고 미접수</span>로 아직 스마트 국민제보 사이트에 민원을 접수하기 전 단계입니다.
       </p>
-      <img src="@/assets/logos/HowToUse2.png" alt="HowToUse2" style="width: 100%;" />
+      <img
+        src="@/assets/logos/HowToUse2.png"
+        alt="HowToUse2"
+        style="width: 100%; border: solid 2px"
+      />
       <p>
         2) 스마트 국민제보 사이트에 민원을 접수하면
-        <span style="color: #007bff">접수 완료</span>로 변경하고, 민원이 처리되면
-        <span style="color: #28a745">처리 완료</span>로 변경합니다.
+        <span style="color: white; background-color: #B6CADA">접수 완료</span>로 변경하고, 민원이 처리되면
+        <span style="color: white; background-color: #0f4c81">처리 완료</span>로 변경합니다.
       </p>
     </div>
     <br />
     <div class="divdiv">
       <h2 style="background-color: #FFFFF0;">2. 민원 상세정보 입력 및 저장</h2>
-      <img src="@/assets/logos/HowToUse3.png" alt="HowToUse3" style="width: 100%;" />
+      <img
+        src="@/assets/logos/HowToUse3.png"
+        alt="HowToUse3"
+        style="width: 100%; border: solid 2px"
+      />
       <p>1) 제보 상세보기에서는 다음의 빈칸들을 작성 후 저장하기를 누릅니다.</p>
-      <img src="@/assets/logos/HowToUse4.png" alt="HowToUse4" style="width: 100%;" />
+      <img
+        src="@/assets/logos/HowToUse4.png"
+        alt="HowToUse4"
+        style="width: 100%; border: solid 2px"
+      />
       <p>
         2) 영상을
-        <span style="color: #28a745">다운로드</span> 받은 후 하단의
-        <span style="color: #17a2b8">스마트 국민제보로 신고하러 가기</span> 버튼을 누릅니다.
+        <span style="color: #0f4c81">다운로드</span> 받은 후 하단의
+        <span style="color: #0f4c81">스마트 국민제보로 신고하러 가기</span> 버튼을 누릅니다.
       </p>
     </div>
     <br />
@@ -43,14 +59,14 @@
       </p>
       <img src="@/assets/logos/HowToUse7.png" alt="HowToUse7" style="width: 100%;" />
       <p>
-        3)분노의민원 사이트 제보목록에
+        3) 분노의민원 사이트 제보목록에
         <span style="color: #28a745">저장된 카드</span>
         를 보면서 1~8번 순서대로 교통위반신고 항목을 기입하고
         <span style="color: #17a2b8">신고하기</span> 버튼을 누릅니다.
       </p>
       <img src="@/assets/logos/HowToUse8.png" alt="HowToUse8" style="width: 100%;" />
       <p>
-        4)신고가
+        4) 신고가
         <span style="color: #28a745">완료</span>됐다면 분노의민원 사이트로 돌아와 사진처럼
         <span style="color: #17a2b8">신고 미접수</span> 상태를
         <span style="color: #17a2b8">접수 완료</span> 상태로 바꿔줍니다.
